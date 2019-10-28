afcd9a4c18a9ea95f16809e0fc59bce3c1612602
unknown <leonardotobe93@gmail.con>
2019-10-27 23:51:08 -0400

    js pro

---
+++ Elprimerplugin/assets/index.js
@@ -0,0 +1,17 @@
+import MediaPlayer from './MediaPlayer.js';
+import AutoPlay from './plugins/AutoPlay.js';
+
+const video = document.querySelector('video');
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] });
+
+const playButton = document.querySelector('#playButton');
+playButton.onclick = () => player.togglePlay();
+
+const muteButton = document.querySelector('#muteButton');
+muteButton.onclick = () => {
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
+};
